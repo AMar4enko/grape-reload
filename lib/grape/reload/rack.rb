ec6eee4c7cef822ebf6055f7b4c136e27a78d05d
AMar4enko <amar4enko@gmail.com>
2014-10-17 20:50:28 +0800

    Few fixes for app reloading to support Rack::Cascade

--- lib/grape/reload/rack.rb
+++ lib/grape/reload/rack.rb
@@ -1,7 +1,7 @@
 module Rack
   class Cascade
     def reinit!
-      @app.map{|app| app.reinit! if app.respond_to?('reinit!') }
+      @apps.map{|app| app.reinit! if app.respond_to?('reinit!') }
     end
   end
 end
